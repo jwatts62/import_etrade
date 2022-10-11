dcaff448dff225b67c0090dc3dfc5511c6ff2cca
John Watts <jwatts62@gmail.com>
2022-10-11 09:45:37 -0500

    Added new file with definitions.

---
+++ transactions.py
@@ -0,0 +1,8 @@
+"""Define transaction records.
+    """
+
+ACTIONS = ['Buy', 'Sell', 'Dividend', 'Return of Capital']
+
+DIVIDEND_TYPES = ['Ordinary', 'Qualified']
+
+CAPITAL_GAIN_TYPES = ['Short Term', 'Long Term']
