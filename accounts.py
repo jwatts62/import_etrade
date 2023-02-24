b52f6c2a670059d43ad5379f01d82be56d3a3253
John Watts <jwatts62@gmail.com>
2023-02-24 09:50:32 -0600

    Added account cross reference table.

---
+++ accounts.py
@@ -0,0 +1,22 @@
+"""This unit provides a cross reference between account
+    numbers, trader, extended account name, and gsheet name.
+    """
+
+accountCrossReference = {
+    'etrade': {
+        '2641': ('etrade-2641-Retirement.gsheet'),
+        '6726': ('etrade-6726-IRA.gsheet'),
+        '9169': ('etrade-9169-Roth-IRA.gsheet')
+        # '': ('etrade--.gsheet')
+    },
+    'fidelity': {
+        'Z211552205': ('Fidelity_brokerage-Z211552205'),
+        '238678824': ('Fidelity-Rollover-IRA-238678824')
+        # '': ('etrade--.gsheet'),
+    }
+    # 'betterment':  {
+    #     '2641': ('etrade-2641-Retirement.gsheet')
+    # }
+}
+
+# End of File
